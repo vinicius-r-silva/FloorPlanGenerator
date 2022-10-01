1c3b8be08dea73547c20bbbc9a7cdbd5f464eda4
vinicius-r-silva <ribeiro.v.silva@gmail.com>
2022-10-01 01:50:43 -0300

    deleted script

--- FloorPlanGenerator/valgrind.sh
+++
@@ -1,8 +0,0 @@
-valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         --log-file=./logs/valgrind-out.txt \
-         ./build/main
-
-cat ./logs/valgrind-out.txt
