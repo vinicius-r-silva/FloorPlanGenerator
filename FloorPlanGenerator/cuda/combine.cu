2ecad6b83ca603b04bddc465e2e66fb772ead52e
vinicius ribeiro da silva <ribeiro.v.silva@gmail.com>
2023-03-25 20:08:35 -0300

    cuda testing

--- FloorPlanGenerator/cuda/combine.cu
+++ FloorPlanGenerator/cuda/combine.cu
@@ -109,13 +109,11 @@ void k_createPts(int16_t *d_a, int16_t *d_b, int16_t *d_res, int8_t *d_nbr, cons
 	if(b_idx >= qtd_b || blockIdx.y >= qtd_a)
 		return;
 
-
 	__shared__ int16_t a[__SIZE_A];
 	if(threadIdx.x < __SIZE_A){
 		a[threadIdx.x] = d_a[a_idx + threadIdx.x];
 	}
   	__syncthreads();
-
 
 	int16_t b[__SIZE_B];
 	for(int i = 0; i < __SIZE_B; i++){
@@ -279,12 +277,17 @@ void k_createPts(int16_t *d_a, int16_t *d_b, int16_t *d_res, int8_t *d_nbr, cons
 	d_res[res_idx + 3] = minH;
 	d_res[res_idx + 4] = minW;
 
-	for(int i = 0; i < __SIZE_NBR; i++){
-		d_nbr[i + nbr_idx] = Neighbors[i];
-	}
+	// for(int i = 0; i < __SIZE_NBR; i++){
+	// 	d_nbr[i + nbr_idx] = Neighbors[i];
+	// }
 	// printf("a_idx: %d,\tb_idx: %d,\tres_idx: %d,\tblockIdx.x: %d,\tblockIdx.y: %d,\tblockIdx.z: %d,\tdblockDim.x: %d,\tthreadIdx.x: %d\n", a_idx, b_idx, res_idx, blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x);
 	// printf("a_idx: %d,\tb_idx: %d,\tres_idx: %d,\tblockIdx.x: %d,\tblockIdx.y: %d,\tblockIdx.z: %d,\tdblockDim.x: %d,\tthreadIdx.x: %d,\tdiffX: %d,\tdiffY: %d,\ta[0]: %d,\ta[1]: %d\n", a_idx, b_idx, res_idx - __SIZE_A, blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, diffX, diffY, a[0], a[1]);
 }
+
+// __global__ 
+// void k_hellowWorld() {
+// 	printf("Hello World. \n\t blockDim.x: %d, blockDim.y: %d, blockDim.z: %d\n\t blockIdx.x: %d, blockIdx.y: %d, blockIdx.z: %d\n\t threadIdx.x: %d, threadIdx.y: %d, threadIdx.z: %d\n", blockDim.x, blockDim.y, blockDim.z, blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z);
+// }
 
 void gpuHandler::createPts(const std::vector<int16_t>& a, const std::vector<int16_t>& b) {
 	const int NConn = 12;
@@ -292,36 +295,24 @@ void gpuHandler::createPts(const std::vector<int16_t>& a, const std::vector<int1
 	const int qtd_a = a.size() / __SIZE_A;
 	const int qtd_b = b.size() / __SIZE_B;
 
-	// const long NConn = 2;
-	// const long qtd_a = 2; 
-	// const long qtd_b = 12; //minimum 12
-	// const long num_a = qtd_a;
-
 	findCudaDevice();	
 
 	const long aLayoutSize = sizeof(int16_t) * __SIZE_A;
 	const long bLayoutSize = sizeof(int16_t) * __SIZE_B;
-	// const long ptsLayoutSize = sizeof(int16_t) * __SIZE_PTS;
 	const long resLayoutSize = sizeof(int16_t) * __SIZE_RES;
 	const long nbrLayoutSize = sizeof(int8_t) * __SIZE_NBR;
 	const unsigned long mem_size_a = aLayoutSize * qtd_a;
 	const unsigned long mem_size_b = bLayoutSize * qtd_b;
-	// const unsigned long mem_size_pts = num_a * NConn * qtd_b * ptsLayoutSize;
 	const unsigned long mem_size_res = num_a * NConn * qtd_b * resLayoutSize;
 	const unsigned long mem_size_nbr = num_a * NConn * qtd_b * nbrLayoutSize;
-	
+
 	// allocate host memory
 	int16_t *h_a = (int16_t *)(&a[0]);
 	int16_t *h_b = (int16_t *)(&b[0]);
-	// int16_t *h_pts = (int16_t *)malloc(mem_size_pts);
-	// int16_t *h_res = (int16_t *)malloc(mem_size_res);
-	// int8_t *h_nbr = (int8_t *)malloc(mem_size_nbr);
- 	// int8_t *h_nbr = nullptr;
 	int16_t *h_res = nullptr;
 
 	// cudaMallocHost((void**)&h_nbr, mem_size_nbr);
 	cudaMallocHost((void**)&h_res, mem_size_res);
-
 
 	// Allocate CUDA events that we'll use for timing
 	cudaEvent_t start, stop;
@@ -332,57 +323,51 @@ void gpuHandler::createPts(const std::vector<int16_t>& a, const std::vector<int1
 	const int num_threads = 768; // 1024
 	const int num_blocks = (qtd_b + num_threads -1) / num_threads;
 
-	dim3 grid(num_blocks, num_a, NConn);
-	dim3 threads(num_threads, 1, 1);
+	// dim3 grid(num_blocks, num_a, NConn);
+	// dim3 threads(num_threads, 1, 1);
+	dim3 grid(2, 2, NConn);
+	dim3 threads(32, 1, 1);
+
 
 	// allocate device memory
-	// int16_t *d_a, *d_b, *d_pts, *d_res;
 	int16_t *d_a, *d_b, *d_res;
 	int8_t *d_nbr;
 	checkCudaErrors(cudaMalloc((void **)&d_a, mem_size_a));
 	checkCudaErrors(cudaMalloc((void **)&d_b, mem_size_b));
-	// checkCudaErrors(cudaMalloc((void **)&d_pts, mem_size_pts));
 	checkCudaErrors(cudaMalloc((void **)&d_res, mem_size_res));
-	// checkCudaErrors(cudaMalloc((void **)&d_nbr, mem_size_nbr));
 
 	// copy host data to device
   	checkCudaErrors(cudaEventRecord(start));
 	checkCudaErrors(cudaMemcpy(d_a, h_a, mem_size_a, cudaMemcpyHostToDevice));
 	checkCudaErrors(cudaMemcpy(d_b, h_b, mem_size_b, cudaMemcpyHostToDevice));
 
-	for(int i = 0; i < qtd_a; i += num_a){
-		int diff = qtd_a - i;
-		if(diff < num_a){
-			k_createPts<<<grid, threads>>>(d_a, d_b, d_res, d_nbr, diff, qtd_b, i);
-			// cudaMemcpy(h_res, d_res, mem_size_res, cudaMemcpyDeviceToHost);
-		} else {
-			k_createPts<<<grid, threads>>>(d_a, d_b, d_res, d_nbr, num_a, qtd_b, i);
-			// cudaMemcpy(h_res, d_res, mem_size_res, cudaMemcpyDeviceToHost);
-		}
-	}
+	// k_hellowWorld<<<grid, threads>>>();
+	k_createPts<<<grid, threads>>>(d_a, d_b, d_res, d_nbr, num_a, qtd_b, 0);
+	// for(int i = 0; i < qtd_a; i += num_a){
+	// 	int diff = qtd_a - i;
+	// 	if(diff < num_a){
+	// 		k_createPts<<<grid, threads>>>(d_a, d_b, d_res, d_nbr, diff, qtd_b, i);
+	// 		// cudaMemcpy(h_res, d_res, mem_size_res, cudaMemcpyDeviceToHost);
+	// 	} else {
+	// 		k_createPts<<<grid, threads>>>(d_a, d_b, d_res, d_nbr, num_a, qtd_b, i);
+	// 		// cudaMemcpy(h_res, d_res, mem_size_res, cudaMemcpyDeviceToHost);
+	// 	}
+	// }
 
-	// k_createPts<<<grid, threads>>>(d_a, d_b, d_pts, num_a, qtd_b, 0);
-	// k_createPts<<<grid, threads>>>(d_a, d_b, d_res, num_a, qtd_b, 0);
-	// k_checkOverlap<<<1, num_threads>>>(d_pts, d_res, qtd_b);
-
-	// cudaDeviceSynchronize();	
+	cudaDeviceSynchronize();
   	checkCudaErrors(cudaEventRecord(stop));
   	checkCudaErrors(cudaEventSynchronize(stop));
 
   	float msecTotal = 0.0f;
   	checkCudaErrors(cudaEventElapsedTime(&msecTotal, start, stop));
 
-	// std::cout << "mem_size_a: " << mem_size_a << ", mem_size_b: " << mem_size_b << ", mem_size_pts: " << mem_size_pts << ", mem_size_res: " << mem_size_res << std::endl;
-	// std::cout << "mem_size_a (MB): " << ((float)mem_size_a)/1024.0/1024.0 << ", mem_size_b (MB): " << ((float)mem_size_b)/1024.0/1024.0 << ", mem_size_pts (MB): " << ((float)mem_size_pts)/1024.0/1024.0 << ", mem_size_res (MB): " << ((float)mem_size_res)/1024.0/1024.0 << std::endl;
-	std::cout << "mem_size_a: " << mem_size_a << ", mem_size_b: " << mem_size_b << ", mem_size_res: " << mem_size_res << ", mem_size_nbr: " << mem_size_nbr << std::endl;
-	std::cout << "mem_size_a (MB): " << ((float)mem_size_a)/1024.0/1024.0 << ", mem_size_b (MB): " << ((float)mem_size_b)/1024.0/1024.0 << ", mem_size_res (MB): " << ((float)mem_size_res)/1024.0/1024.0 << ", mem_size_nbr (MB): " << ((float)mem_size_nbr)/1024.0/1024.0 << std::endl;
-
 	std::cout << "a.size(): " << a.size() << ", b.size(): " << b.size() << std::endl;
 	std::cout << "qtd_a: " << qtd_a << ", qtd_b: " << qtd_b << std::endl;
-
 	std::cout << "num_threads: " << num_threads << ", num_blocks: " << num_blocks << std::endl;
 	std::cout << "grid: " << grid.x << ", " << grid.y << ", " << grid.z << std::endl;
 	std::cout << "threads: " << threads.x << ", " << threads.y << ", " << threads.z << std::endl;
+	std::cout << "mem_size_a: " << mem_size_a << ", mem_size_b: " << mem_size_b << ", mem_size_res: " << mem_size_res << ", mem_size_nbr: " << mem_size_nbr << std::endl;
+	std::cout << "mem_size_a (MB): " << ((float)mem_size_a)/1024.0/1024.0 << ", mem_size_b (MB): " << ((float)mem_size_b)/1024.0/1024.0 << ", mem_size_res (MB): " << ((float)mem_size_res)/1024.0/1024.0 << ", mem_size_nbr (MB): " << ((float)mem_size_nbr)/1024.0/1024.0 << std::endl;
 	std::cout << "Time: " << msecTotal << std::endl;
 
 	// check if kernel execution generated and error
@@ -455,5 +440,5 @@ void gpuHandler::createPts(const std::vector<int16_t>& a, const std::vector<int1
 	// cleanup host memory
 	// free(h_pts);
 	// free(h_res);
-	checkCudaErrors(cudaFree(h_res));
+	checkCudaErrors(cudaFreeHost(h_res));
 }
